6c91546d23d5e03b00c1941518c9646d0a388666
Ryan Alexander <scloopy@gmail.com>
2011-05-06 04:10:45 -0700

    Add Cube example

---
+++ examples/cube/cube.glsl
@@ -0,0 +1,30 @@
+varying vec3 v_normal;
+varying vec2 v_texcoord;
+
+
+#ifdef VERTEX
+
+uniform mat4 u_modelview, u_projection;
+
+attribute vec3 a_position, a_normal;
+attribute vec2 a_texcoord;
+
+void main()
+{
+	v_texcoord = a_texcoord;
+    v_normal   = ((u_modelview * vec4(a_normal, 0.0)).xyz + 1.0) * 0.5;
+
+    gl_Position = u_projection * u_modelview * vec4(a_position, 1.0);
+}
+
+#endif
+
+
+#ifdef FRAGMENT
+
+void main()
+{
+	gl_FragColor = vec4(v_normal, 1.0);
+}
+
+#endif
---
+++ examples/cube/cube.js
@@ -0,0 +1,53 @@
+var fs = require('fs')
+,   plask = require('plask')
+,   em = require('../../embr');
+
+plask.simpleWindow({
+    settings: {
+        title:   "CuBe",
+        type:    "3d",
+        width:   512,
+        height:  512,
+        vsync:   true,
+        center:  true
+    },
+
+    init: function()
+    {
+        this.framerate(60);
+
+        this.projection = new em.Mat4().perspective(60, this.width / this.height, 0.01, 8);
+        this.modelview  = new em.Mat4().lookAt( 0, 0,-4,  // Eye Position
+                                                0, 0, 0,  // Target Position
+                                                0, 1, 0); // Up Vector
+
+        var gl = this.gl;
+        gl.enable(gl.DEPTH_TEST)
+
+        // Make Shader
+        this.prog = new em.MagicProgram(gl, em.makeProgram(gl, "cube.glsl"));
+
+        // Make Cube
+        this.cube = em.makeCube(gl, 1, 1, 1, this.prog.loc_a_position,
+                                             this.prog.loc_a_normal,
+                                             this.prog.loc_a_texcoord);
+    },
+
+    draw: function()
+    {
+        var gl = this.gl
+
+        gl.clearColor(0,0,0,1);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        this.modelview.rotate(Math.sin(this.frametime) * 0.03, 0, 1, 0);
+        this.modelview.rotate(Math.cos(this.frametime) * 0.07, 1, 0, 0);
+
+        var prog = this.prog;
+        prog.useProgram();
+        prog.set_u_modelview(this.modelview);
+        prog.set_u_projection(this.projection);
+
+        this.cube.draw();
+    }
+});
