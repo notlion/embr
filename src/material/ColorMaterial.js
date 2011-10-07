9c2615086fffbbf50f4b75c62bf74ac6d526086e
Ryan Alexander <scloopy@gmail.com>
2011-10-06 23:01:38 -0700

    Use comma first style for glsl source strings

--- src/material/ColorMaterial.js
+++ src/material/ColorMaterial.js
@@ -1,53 +1,53 @@
 Embr.ColorMaterial = (function(){
 
-    var src_vertex = [
-        "uniform mat4 modelview, projection;",
+    var src_vertex =
+    [   "uniform mat4 modelview, projection;"
 
-        "attribute vec3 a_position;",
+    ,   "attribute vec3 a_position;"
 
-        "#ifdef use_vertex_color",
-            "attribute vec4 a_color;",
-            "varying vec4 v_color;",
-        "#endif",
+    ,   "#ifdef use_vertex_color"
+    ,       "attribute vec4 a_color;"
+    ,       "varying vec4 v_color;"
+    ,   "#endif"
 
-        "#ifdef use_texture",
-            "attribute vec2 a_texcoord;",
-            "varying vec2 v_texcoord;",
-        "#endif",
+    ,   "#ifdef use_texture"
+    ,       "attribute vec2 a_texcoord;"
+    ,       "varying vec2 v_texcoord;"
+    ,   "#endif"
 
-        "void main(){",
-            "#ifdef use_vertex_color",
-                "v_color = a_color;",
-            "#endif",
-            "#ifdef use_texture",
-                "v_texcoord = a_texcoord;",
-            "#endif",
-            "gl_Position = projection * modelview * vec4(a_position, 1.0);",
-        "}"
+    ,   "void main(){"
+    ,       "#ifdef use_vertex_color"
+    ,           "v_color = a_color;"
+    ,       "#endif"
+    ,       "#ifdef use_texture"
+    ,           "v_texcoord = a_texcoord;"
+    ,       "#endif"
+    ,       "gl_Position = projection * modelview * vec4(a_position, 1.0);"
+    ,   "}"
     ].join("\n");
 
-    var src_fragment = [
-        "uniform vec4 color;",
+    var src_fragment =
+    [   "uniform vec4 color;"
 
-        "#ifdef use_vertex_color",
-            "varying vec4 v_color;",
-        "#endif",
+    ,   "#ifdef use_vertex_color"
+    ,       "varying vec4 v_color;"
+    ,   "#endif"
 
-        "#ifdef use_texture",
-            "uniform sampler2D texture;",
-            "varying vec2 v_texcoord;",
-        "#endif",
+    ,   "#ifdef use_texture"
+    ,       "uniform sampler2D texture;"
+    ,       "varying vec2 v_texcoord;"
+    ,   "#endif"
 
-        "void main(){",
-            "vec4 c = color;",
-            "#ifdef use_vertex_color",
-                "c *= v_color;",
-            "#endif",
-            "#ifdef use_texture",
-                "c *= texture2D(texture, v_texcoord);",
-            "#endif",
-            "gl_FragColor = c;",
-        "}"
+    ,   "void main(){"
+    ,       "vec4 c = color;"
+    ,       "#ifdef use_vertex_color"
+    ,           "c *= v_color;"
+    ,       "#endif"
+    ,       "#ifdef use_texture"
+    ,           "c *= texture2D(texture, v_texcoord);"
+    ,       "#endif"
+    ,       "gl_FragColor = c;"
+    ,   "}"
     ].join("\n");
 
 
--- src/material/NormalMaterial.js
+++ src/material/NormalMaterial.js
@@ -1,26 +1,26 @@
 Embr.NormalMaterial = (function(){
 
-    var src_vertex = [
-        "uniform mat4 modelview, projection;",
+    var src_vertex =
+    [   "uniform mat4 modelview, projection;"
 
-        "attribute vec3 a_position;",
-        "attribute vec3 a_normal;",
+    ,   "attribute vec3 a_position;"
+    ,   "attribute vec3 a_normal;"
 
-        "varying vec4 v_color;",
+    ,   "varying vec4 v_color;"
 
-        "void main(){",
-            "vec4 normal = modelview * vec4(a_normal, 0.0);",
-            "v_color = vec4((normal.xyz + 1.0) * 0.5, 1.0);",
-            "gl_Position = projection * modelview * vec4(a_position, 1.0);",
-        "}"
+    ,   "void main(){"
+    ,       "vec4 normal = modelview * vec4(a_normal, 0.0);"
+    ,       "v_color = vec4((normal.xyz + 1.0) * 0.5, 1.0);"
+    ,       "gl_Position = projection * modelview * vec4(a_position, 1.0);"
+    ,   "}"
     ].join("\n");
 
-    var src_fragment = [
-        "varying vec4 v_color;",
+    var src_fragment =
+    [   "varying vec4 v_color;"
 
-        "void main(){",
-            "gl_FragColor = v_color;",
-        "}"
+    ,   "void main(){"
+    ,       "gl_FragColor = v_color;"
+    ,   "}"
     ].join("\n");
 
 
