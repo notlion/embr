cae73feb13f1ce5d85f65c83e7ad449dab8cc416
Ryan Alexander <scloopy@gmail.com>
2011-12-15 03:19:50 -0800

    Refactor Texture to allow setting data from an <img>

--- src/core/Texture.js
+++ src/core/Texture.js
@@ -4,34 +4,58 @@ define(function(){
 
     "use strict";
 
-    function Texture(gl, width, height, data, fmt){
-        this.gl     = gl;
-        this.width  = width;
-        this.height = height;
-
-        if(fmt === undefined) fmt = {};
-
-        var filter_min = fmt.filter_min !== undefined ? fmt.filter_min : gl.NEAREST
-        ,   filter_mag = fmt.filter_mag !== undefined ? fmt.filter_mag : gl.NEAREST
-        ,   wrap_s     = fmt.wrap_s     !== undefined ? fmt.wrap_s     : gl.CLAMP_TO_EDGE
-        ,   wrap_t     = fmt.wrap_t     !== undefined ? fmt.wrap_t     : gl.CLAMP_TO_EDGE;
-
-        this.target  = fmt.target  !== undefined ? fmt.target  : gl.TEXTURE_2D;
-        this.format  = fmt.format  !== undefined ? fmt.format  : gl.RGBA;
-        this.formati = fmt.formati !== undefined ? fmt.formati : gl.RGBA;
-        this.type    = fmt.type    !== undefined ? fmt.type    : gl.UNSIGNED_BYTE;
-        this.unit    = fmt.unit    !== undefined ? fmt.unit    : gl.TEXTURE0;
-
+    function Texture(gl){
+        this.gl = gl;
         this.handle = gl.createTexture();
-        gl.bindTexture(this.target, this.handle);
-        gl.texImage2D(this.target, 0, this.formati, width, height, 0, this.format, this.type, data);
-        gl.texParameteri(this.target, gl.TEXTURE_MIN_FILTER, filter_min);
-        gl.texParameteri(this.target, gl.TEXTURE_MAG_FILTER, filter_mag);
-        gl.texParameteri(this.target, gl.TEXTURE_WRAP_S, wrap_s);
-        gl.texParameteri(this.target, gl.TEXTURE_WRAP_T, wrap_t);
     }
 
     Texture.prototype = {
+
+        dispose: function(){
+            gl.deleteTexture(this.handle);
+        },
+
+        updateFormat: function(fmt){
+            var gl = this.gl;
+
+            if(typeof fmt != "object")
+                fmt = {};
+
+            this.target     = fmt.target     || gl.TEXTURE_2D;
+            this.format     = fmt.format     || gl.RGBA;
+            this.formati    = fmt.formati    || gl.RGBA;
+            this.type       = fmt.type       || gl.UNSIGNED_BYTE;
+            this.filter_min = fmt.filter_min || gl.NEAREST;
+            this.filter_mag = fmt.filter_mag || gl.NEAREST;
+            this.wrap_s     = fmt.wrap_s     || gl.CLAMP_TO_EDGE;
+            this.wrap_t     = fmt.wrap_t     || gl.CLAMP_TO_EDGE;
+            this.unit       = fmt.unit >= 0 ? gl.TEXTURE0 + fmt.unit : gl.TEXTURE0;
+
+            gl.bindTexture(this.target, this.handle);
+            gl.texParameteri(this.target, gl.TEXTURE_MIN_FILTER, this.filter_min);
+            gl.texParameteri(this.target, gl.TEXTURE_MAG_FILTER, this.filter_mag);
+            gl.texParameteri(this.target, gl.TEXTURE_WRAP_S, this.wrap_s);
+            gl.texParameteri(this.target, gl.TEXTURE_WRAP_T, this.wrap_t);
+        },
+
+        setDataWithElement: function(element, fmt){
+            var gl = this.gl;
+            this.updateFormat(fmt);
+            // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+            gl.texImage2D(this.target, 0, this.formati, this.format, this.type, element);
+        },
+
+        setData: function(width, height, data, fmt){
+            var gl = this.gl;
+            this.updateFormat(fmt);
+            gl.texImage2D(this.target, 0, this.formati, width, height, 0, this.format, this.type, data);
+        },
+
+        updateData: function(data){
+            var gl = this.gl;
+            gl.bindTexture(this.target, this.handle);
+            gl.texSubImage2D(this.target, 0, 0, 0, this.width, this.height, this.format, this.type, data);
+        },
 
         bind: function(unit){
             var gl = this.gl;
@@ -44,12 +68,6 @@ define(function(){
             var gl = this.gl;
             gl.activeTexture(this.unit);
             gl.bindTexture(this.target, null);
-        },
-
-        update: function(data){
-            var gl = this.gl;
-            gl.bindTexture(this.target, this.handle);
-            gl.texSubImage2D(this.target, 0, 0, 0, this.width, this.height, this.format, this.type, data);
         }
 
     };
